5c8440f12bb7881ab2821c3e6650b57243e48505
Doeun Kwon <dekwooon@gmail.com>
2024-12-01 22:25:45 -0800

    prepare for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <main className="App">
-      <LandingPage />
-    </main>
+    <Router basename={process.env.PUBLIC_URL}>
+      <main className="App">
+        <LandingPage />
+      </main>
+    </Router>
   );
 }
 
